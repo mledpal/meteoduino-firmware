c6b393a5930c2923d7a12cd11098fed2ba732aa2
mledpal <mledpal@gmail.com>
2024-11-25 18:57:37 +0100

    Ida de olla del Code

--- MeteoDuinoReader/src/meteoduinoreader/MeteoDuinoReader.java
+++ MeteoDuinoReader/src/meteoduinoreader/MeteoDuinoReader.java
@@ -1,15 +1,5 @@
 package meteoduinoreader;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -26,6 +16,15 @@ import java.sql.Statement;
 import java.text.ParseException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 /**
  * @author Miguel Ledesma Palacios
